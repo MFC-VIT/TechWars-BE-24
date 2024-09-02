06af7e09884bcf27b985e22d7a4e25c8adb79bcf
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-09-03 00:41:43 +0530

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+import "dotenv/config"
+import appRouter from "./routes/index.js"
+import { connectDB } from "./db/connectDB.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import logger from "./middlewares/logger.js";
+
+const PORT = process.env.PORT || 8080;
+const dbConnectionString = process.env.MONGO_URI || "";
+
+connectDB(dbConnectionString);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(logger);
+
+app.get('/', (req, res) => res.send("Welcome to Tech Wars BE"));
+
+app.use("/api/v1", appRouter);
+
+app.use(errorHandler);
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Error 404: Route does not exist"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening to the PORT ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import teamRouter from "./teamRoutes.js";
 
 const router = Router();
-
-router.use("/team", teamRouter);
 
 export default router;
