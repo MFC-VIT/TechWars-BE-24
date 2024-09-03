079b78a63bcc61a3d61e04ea5dd95bcd9f1be594
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-09-03 07:05:42 +0530

    intialModels

---
+++ controllers/lobbyController.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const lobby = require('../models/gameLobbyModel');
+
+exports.createLobby = async (req,res) =>{
+    try{
+        const newLobby = new lobby(req.body);
+        await newLobby.save();
+        res.status(201).json({message: "new lobby created"});
+    }
+    catch{
+        res.status(500).json({error: error.message});
+    }
+}
--- models/territoryModel.js
+++ models/territoryModel.js
@@ -4,18 +4,44 @@
  * Owner ID
 */
 
-import { Schema } from "mongoose"
+// import { Schema } from "mongoose"
 
-const territorySchema = new Schema({
-    name: {
-        type: String,
-    },
-    lobby: {
+// const territorySchema = new Schema({
+//     name: {
+//         type: String,
+//     },
+//     lobby: {
         
-    },
-    troops: {
+//     },
+//     troops: {
         
-    }
+//     }
 
     
-})
+// })
+const mongoose = require('mongoose');
+
+const TerritorySchema = new mongoose.Schema({
+    territory_id:{
+        type:mongoose.Schema.ObjectId,
+        required:true
+    },
+    territoryName: {
+        type: String,
+        required: true
+    },
+    lobbyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Lobby'
+    },
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    troops_deployed:{
+        type:Number,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Territory', TerritorySchema);
