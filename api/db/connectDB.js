4a73d9fd997739d71d83c07244df3703e1dc0344
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-09-27 09:55:21 +0530

    fix

--- api/db/connectDB.js
+++ api/db/connectDB.js
@@ -4,7 +4,7 @@ export const connectDB = async (dbConnectionString) => {
   try {
     const connection = await mongoose.connect(dbConnectionString);
     console.log(`Successfully connected to database`);
-    console.log(`HOST: ${connection.connection.host}`);
+    // console.log(`HOST: ${connection.connection.host}`);
     console.log(`PORT: ${connection.connection.port}`);
     console.log(`NAME: ${connection.connection.name}\n`);
   } catch (error) {
